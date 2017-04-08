64cdc23a0816cd41fa309a1454b2bf4207b9b378
shachar <shachar3210@gmail.com>
2017-04-08 12:43:10 +0300

    add card class and player calss

---
+++ TexasHoldem_1/TexasHoldem_1/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem_1
+{
+    public enum CardType
+    {
+        Spades = 1, //עלה
+        Hearts = 2,//לב
+        Diamonds = 3,// יהלום
+        Clubs = 4, // תלתן
+    }
+    class Card
+    {
+      
+        public int value;
+        public int type;
+
+        public Card(int value, int type)
+        {
+            if (value < 1 || value > 13 || type < 1 || type > 4)
+            {
+                throw new Exception("illegal card");
+            }
+
+            this.value = value;
+            this.type = type;
+        }
+    }
+
+}
+
---
+++ TexasHoldem_1/TexasHoldem_1/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexasHoldem_1
+{
+    class Player
+    {
+        public Card[] hand = new Card[2];
+        public string name;
+        
+        public Player(string name)
+        {
+            if (name == null) throw new Exception("illegal player name");
+
+            this.name = name;
+        }
+
+        public void setCards(Card first, Card second)
+        {
+           
+            if (first == null || second == null) throw new Exception("can't put null cards");
+            this.hand[0] = first;
+            this.hand[1] = second;
+        }
+       
+        
+    }
+}
